efb2278854ccd94e7c17ea8637f1c3b66469f582
XieSanqiu <xll_sanqiu@163.com>
2020-10-04 15:57:18 +0800

    feat:python基础——基础语法

---
+++ basic/BasicGrammar.py
@@ -0,0 +1,22 @@
+# 条件判断
+age = 3
+if age >= 18:
+    print('adult')
+elif age >= 6:
+    print('teenager')
+else:
+    print('kid')
+
+# 循环
+# for in 循环
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
+
+# while循环
+sum = 0
+n = 99
+while n > 0:
+    sum = sum + n
+    n = n - 2
+print(sum)
