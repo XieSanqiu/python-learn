b3c16f696f0bc4a2f602cb34b84b7941fbd25a55
XieSanqiu <xll_sanqiu@163.com>
2020-10-16 16:23:10 +0800

    feat:初始化进程活动信息

--- process_activity/Init.py
+++ process_activity/Init.py
@@ -1,52 +1,78 @@
 '''
 初始化进程活动信息
-以 主机IP+进程名+进程id 为主键确定一次进程活动信息
+以 主机IP+进程名+进程参数+进程id+进程启动时间 为主键确定一次进程活动信息
 其中后面需要更新的是 max_cpu、avg_cpu、max_memory、avg_memory
 '''
 import pymongo
 from common.GetProcessInfo import query_by_index as es_query
+import math
 
 my_client = pymongo.MongoClient("mongodb://211.65.197.70:27017/")
 
 def init():
     hosts = ['211.65.193.23', '211.65.197.175', '211.65.197.233']
     today = '2020.10.15'
-    my_collection = my_client['pinfo']['activity']
-    count = 1
+
     for host in hosts:
         print('-------------------------' + host + '-------------------------')
         index = host + '-pinfo-' + today
         process_infos = es_query(index)
+        rem = dict()
         for one in process_infos:
+            # 主机IP+进程名+进程参数+进程id+进程启动时间 唯一确定一次进程活动
             proc_name = one['_source']['proc_name']
+            proc_param = one['_source']['proc_param']
             proc_pid = one['_source']['pid']
+            start_time = one['_source']['start_time']
 
-            my_query = {"host_ip":host, "proc_name":proc_name, "proc_pid":proc_pid} #主机IP+进程名+进程id唯一确定一次进程活动
-            doc_count = my_collection.count_documents(my_query)
-            if doc_count == 0:
-                mongo_one = es_2_mongodb_add(one)
-                my_collection.insert_one(mongo_one)
-                count += 1
-                if count % 100 == 0:
-                    print(count)
+            key = (host, proc_name, proc_param, proc_pid, start_time)
+
+            mg_one = es_2_mg(one)
+
+            if key not in rem:
+                rem[key] = mg_one
+                rem[key]['cpu_count'] = 1
+                rem[key]['memory_count'] = 1
+            else:
+                rem[key]['max_cpu'] = max(rem[key]['max_cpu'], one['_source']['cpu_percent'])
+                rem[key]['avg_cpu'] = (rem[key]['avg_cpu'] * rem[key]['cpu_count'] + one['_source']['cpu_percent']) \
+                                      / (rem[key]['cpu_count'] + 1)
+                rem[key]['cpu_count'] += 1
+
+                rem[key]['max_memory'] = max(rem[key]['max_memory'], one['_source']['mem_percent'])
+                rem[key]['avg_memory'] = (rem[key]['avg_memory'] * rem[key]['memory_count'] + one['_source']['mem_percent']) \
+                                         / (rem[key]['memory_count'] + 1)
+                rem[key]['memory_count'] += 1
+
+                if 'files' in mg_one:
+                    if 'files' not in rem:
+                        rem[key]['files'] = list()
+                    for file in mg_one['files']:
+                        if file not in rem[key]['files']:
+                            rem[key]['files'].append(file)
+                if 'sockets' in mg_one:
+                    if 'sockets' not in rem:
+                        rem[key]['sockets'] = list()
+                    for socket in mg_one['sockets']:
+                        if socket not in rem[key]['sockets']:
+                            rem[key]['sockets'].append(socket)
+        write_2_mongodb(rem)
+
 
 # 将es里的信息格式转为mongodb里的格式
-def es_2_mongodb_add(es_one):
+def es_2_mg(es_one):
     mongo_one = dict()
     mongo_one['host_ip'] = es_one['_source']['HostIP']
     mongo_one['proc_name'] = es_one['_source']['proc_name']
     mongo_one['proc_pid'] = es_one['_source']['pid']
     mongo_one['start_time'] = es_one['_source']['start_time'] #时间戳
-    mongo_one['update_time'] = es_one['_source']['start_time']
     mongo_one['user_name'] = es_one['_source']['user_name']
     mongo_one['proc_param'] = es_one['_source']['proc_param'] #进程启动参数
 
     mongo_one['max_cpu'] = es_one['_source']['cpu_percent']
-    mongo_one['min_cpu'] = es_one['_source']['cpu_percent']
     mongo_one['avg_cpu'] = es_one['_source']['cpu_percent']
 
     mongo_one['max_memory'] = es_one['_source']['mem_percent']
-    mongo_one['min_memory'] = es_one['_source']['mem_percent']
     mongo_one['avg_memory'] = es_one['_source']['mem_percent']
 
     sockets_num = es_one['_source']['connection_num'] #进程没有网络连接，则没有对应key
@@ -67,14 +93,10 @@ def es_2_mongodb_add(es_one):
 
     return mongo_one
 
+def write_2_mongodb(rem):
+    my_collection = my_client['pinfo']['activity']
+    for one in rem:
+        my_collection.insert_one(rem[one])
 
 if __name__ == '__main__':
     init()
-    # my_collection = my_client['pinfo']['activity']
-    # my_query = {"proc_name": "apache2", "proc_pid": 3133}
-    # doc_count = my_collection.count_documents(my_query)
-    # print(doc_count)
-
-    # 清空集合
-    # x = my_collection.delete_many({})
-    # print(x.deleted_count)
---
+++ process_activity/test.py
@@ -0,0 +1,19 @@
+'''
+用于一些测试
+'''
+import pymongo
+
+my_client = pymongo.MongoClient("mongodb://211.65.197.70:27017/")
+
+my_collection = my_client['pinfo']['activity']
+
+
+# Mongodb 查询数据
+# my_query = {"host_ip":"211.65.197.175", "proc_name": "apache2"}
+# results = my_collection.find(my_query)
+# for res in results:
+#     print(res)
+
+# 清空集合
+x = my_collection.delete_many({})
+print(x.deleted_count)
